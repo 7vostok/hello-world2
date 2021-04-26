d3172d98cc2c66377c00609b32f267520f923076
7vostok <zach1.rags@gmail.com>
2021-04-26 15:35:12 -0700

    Added hello world print, added description including author and date to the Main class file

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,4 +1,16 @@
+/*
+ * Lab4 Hello World
+ * Author Zach Ragsdale
+ * 4/26/2021
+ */
+
 package edu.pcc.cis.helloworld;
 
 public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("This was made on April 26, 2021");
+    }
+
 }
